484e2a7ba571a2416842aded9b82db0f04c53281
temirlan-amanzhol <krabytime@mail.ru>
2025-09-16 20:03:21 +0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,68 @@
+#print 1.
+print ("hi fella")
+
+#plus 2.
+x = 4
+y = 5
+print(x + y)
+
+#plus strings 3.
+z = "tima"
+t = " is good"
+print(z + t)
+
+#type definition 4.
+r = "rad"
+print(type(r))
+
+#condition 5.
+ni = 5
+gi = 6
+
+if ni > gi:
+    print("ni is greater")
+elif ni == gi:
+    print("equal")
+else:
+    print("gi is bigger")
+
+#float 6.
+f = 5.5
+print(float(f))
+
+#list 7.
+t = ["banana", "monkey", "egg"]
+print(t[0])
+
+#multiple variables 8.
+gu, tu, yu = "break" , "leg", "screw"
+print(gu)
+print(tu)
+print(yu)
+
+#function 9.
+hi = "awesome"
+
+def myfunc():
+    print("I am so" + hi)
+myfunc()
+
+#string 10.
+st = str(5000)
+print(st)
+
+#minus 11.
+op = 8
+pr = 5
+print(op - pr)
+
+#mult 12.
+ti = 4
+ma = 5
+print(ti * ma)
+
+#div 13.
+
+sa = 54
+na = 6
+print(float(sa / na))
